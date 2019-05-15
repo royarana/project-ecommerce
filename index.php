a652450530ed06d71a2169c37e4cf4d0c110bae8
royarana <royremoto7@gmail.com>
2019-05-15 18:50:11 +0800

    implement get data from POST PUT PATCH

--- index.php
+++ index.php
@@ -12,16 +12,23 @@
 	    return $protocol . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 	}
 
+	function getUrlVar() {
+		$current_url = explode("/", $_SERVER['REQUEST_URI']);
+		$current_url = $current_url[end(array_keys($current_url))];
+		return $current_url;
+	}
+
 	if (strpos(url(), 'index.php/api')) {
 		$requestType = $_SERVER;
 		if ($requestType === "GET") {
 		    $data = $_GET;
 		} else {
 			$content = trim(file_get_contents('php://input'));
-			$decoded = json_decode($content, true);
-			$data = $decoded;
+			parse_str($_SERVER["QUERY_STRING"], $requestUri);
+			$data = json_decode($content, true);
+			$data = array_merge($data, $requestUri);
+			print_r($data);
 		}
-		
 	} else {
 		header('Content-Type: application/json');
 		$uri = $_SERVER['REQUEST_URI'];
