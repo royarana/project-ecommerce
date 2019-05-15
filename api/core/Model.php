a9b6ba40f07b688ff4b7056b23469a44b757da3e
royarana <royremoto7@gmail.com>
2019-05-15 14:48:38 +0800

    added Model.php

---
+++ api/core/Model.php
@@ -0,0 +1,177 @@
+<?php
+
+require './db.php';
+
+
+Class Model {
+	private $_columns = [];
+	private $_conn;
+	private $_connString;
+	public $_table;
+	private $_where = [];
+	private $_values = [];
+	private $_joins = [];
+	private $_select = [];
+
+	function __construct($table) {
+		global $CONFIG;
+		$this->_table = $table;
+		$this->_connString = $CONFIG;
+		$this->_setConnection();
+	}
+
+	function clear() {
+		 $this->_columns = [];
+		 $this->_where = [];
+		 $this->_values = [];
+		 $this->_joins = [];
+		 $this->_select = [];
+	}
+
+	function _setConnection() {
+		$conn = $this->_connString;
+		$this->_conn = new mysqli(
+			$conn["localhost"],
+			$conn["user"],
+			$conn["password"],
+			$conn["db"]
+		);
+
+		if ($this->_conn->connect_error) {
+			die("MYSQL ERROR {mysqli_error()}");
+		}
+	}
+
+	function _setColumns() {
+		$columns = [];
+		$result = $this->_conn->query("
+			SHOW COLUMNS FROM {$this->_table}
+		");
+
+		while($row = $result->fetch_assoc()) {
+		    $columns[] = "{$this->_table}.{$row['Field']}";
+		}
+
+		$this->_columns = $columns;
+	}
+
+	function _getColumns() {
+		return $this->_columns;
+	}
+
+	function where($col, $value, $operator = "=") {
+		$this->_where[] = array( "key" => "AND" , "value" => "{$col} ${operator} ?" );
+		$this->_values[] = $value;
+	}
+
+	function orWhere($col, $value, $operator = "=") {
+		$this->_where[] = array( "key" => "OR" , "value" => "{$col} ${operator} ?" );
+		$this->_values[] = $value;
+	}
+
+	function join($table, $colA, $colB, $operator = "=") {
+		$this->_joins[] = array( "join" => "INNER JOIN {$table} ON", "value" => "${colA} ${operator} {$colB}" );
+	}
+
+	function leftJoin($table, $colA, $colB, $operator = "=") {
+		$this->_joins[] = array( "join" => "LEFT JOIN {$table} ON", "value" => "${colA} ${operator} {$colB}" );
+	}
+
+	function select($cols = "*") {
+		if ($cols !== "") {
+			$this->_columns = explode(", ", $cols);
+		} else {
+			$this->_setColumns();
+		}
+
+		$cols = implode(", ", $this->_columns);
+
+		$this->_select = "SELECT {$cols} FROM {$this->_table} ";
+	}
+
+	function get() {
+		$rows = $this->getRows();
+		return $rows;
+	}
+
+	function getOne() {
+		$rows = $this->getRows();
+
+		if (!empty($rows)) {
+			return array();
+		}
+
+		return $rows[0];
+	}
+
+	function getRows() {
+		$result = [];
+		$joinStr = "";
+		$whereStr = "";
+
+		if (!empty($this->_joins)) {
+
+			foreach( $this->_joins as $join ) {
+				$joinStr .= " {$join["join"]} {$join["value"]}";
+			}
+		}
+
+		if (!empty($this->_where)) {
+			foreach( $this->_where as $where ) {
+				if ($whereStr === "") {
+					$whereStr .= " WHERE {$where["value"]}";
+				} else {
+					$whereStr .= " {$where["key"]} {$where["value"]}";
+				}
+			}
+		}
+
+		$this->_select .= " {$joinStr} {$whereStr}";
+
+		$stmt = $this->_conn->prepare($this->_select);
+
+		//adds value
+		/*
+			i - integer
+			d - double
+			s - string
+			b - BLOB
+		*/
+
+		$params = [];
+
+		if (!empty($this->_values)) {
+			foreach($this->_values as $value ) {
+				$val = strtolower(gettype($value)[0]);
+				$params[] = $val;
+			}
+			$stmt->bind_param(implode("", $params), ...$this->_values);
+		}
+		
+		$stmt->execute();
+		$queryRes = $stmt->get_result();
+
+		while ($data = $queryRes->fetch_assoc()) {
+		    $result[] = $data;
+		}
+
+		return $result;
+	}
+} 
+
+class Test extends Model {
+	function __construct() {
+		parent::__construct("TEST");
+	}
+}
+
+$TestModel = new Test();
+
+	$TestModel->select("test_join.*");
+	$TestModel->where("test.id", 2, ">=");
+	$TestModel->orWhere("test.id", 1, "=");
+	$TestModel->join("join_test", "test.id", "join_test.id");
+	$TestModel->leftJoin("test_join", "test.id", "test_join.id");
+	$result = $TestModel->getRows();
+	print_r($result);
+?>
---
+++ api/core/db.php
@@ -0,0 +1,8 @@
+<?php 
+	$CONFIG = array(
+		"localhost" => "localhost",
+		"user" => "root",
+		"password" => "",
+		"db" => "ecommerce"
+	);
+?>
